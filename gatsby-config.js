9585ac00b1232f235ee5e00c186884fe100d325e
Jakub Zygmunt <216938@edu.p.lodz.pl>
2021-05-12 22:44:17 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    siteMetadata: {
+        title: "KNL 2021",
+    },
+    plugins: [
+        "gatsby-plugin-netlify-cms",
+        "gatsby-plugin-styled-components",
+        "gatsby-plugin-image",
+        {
+            resolve: "gatsby-plugin-google-analytics",
+            options: {
+                trackingId: "UA-80786318-1",
+            },
+        },
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sitemap",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/",
+            },
+            __key: "images",
+        },
+    ],
+};
